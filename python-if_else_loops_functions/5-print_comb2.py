eae66e1c9777bf4d1ab97c0c525aa6c7f6596b70
scardona14 <cardanasean123@gmail.com>
2024-01-23 12:46:13 -0400

    fix typo

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for number in range(0, 100):
     if number < 100:
         print('{:02d}'.format(number), end=", ")
     else:
-        print("{}".formar(number))
+        print("{}".format(number))
