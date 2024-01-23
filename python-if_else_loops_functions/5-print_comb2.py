82ea26f60c4722c2dd2fe750cf2b4f7feacfa762
scardona14 <cardanasean123@gmail.com>
2024-01-23 12:21:12 -0400

    prints numbers from 0 to 99

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number in range(0, 99):
+    print('{:02d}'.format(number), end='')
+    if number != 98:
+        print(', ', end='')
+    else:
+        print('')
+        
