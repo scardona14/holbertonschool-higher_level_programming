bfdf5cac04f0987685cd58224ad657b74e857e3d
scardona14 <cardanasean123@gmail.com>
2024-01-24 15:39:49 -0400

    prints all the names defined by the compiled module hidden_4.pyc

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+# Add the path to the module if it is not in the default Python path
+sys.path.append('/path/to/hidden_4')
+
 if __name__ == "__main__":
-    from main_files import hidden_4
+    import hidden_4
 
     names = dir(hidden_4)
     for name in names:
