0ff4db03386f99e2fbe4b630a2a3d71f692241d5
scardona14 <cardanasean123@gmail.com>
2024-01-29 13:05:27 -0400

    added the def functions

--- 1-search_replace.py
+++ 1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [replace if x == search else x for x in my_list]
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
