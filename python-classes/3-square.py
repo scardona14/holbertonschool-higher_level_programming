28c16e6727aa0819a3ba26de7f1296035b98cf33
scardona14 <cardanasean123@gmail.com>
2024-02-01 11:28:26 -0400

    changed code due error

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
 """
 3. Area of a square
-A class Square that defines a square by: (based on 2-square.py)
+Write a class Square that defines a square by: (based on 2-square.py)
 """
-
 class Square:
-    """" A class that defines a square by size"""
-
+    """Class Square that defines a square"""
     def __init__(self, size=0):
-        """"
-        Define private instance attribute: size
-        Raise TypeError & ValueError if not int or <0 resp
-        """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        
+        """Define Private instance attribute: size"""
         self.__size = size
-
-        def area(self):
-            """"Function that calculates area"""
-
-            return (self.__size ** 2)
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """Returns the current square area"""
+        return self.__size * self.__size
