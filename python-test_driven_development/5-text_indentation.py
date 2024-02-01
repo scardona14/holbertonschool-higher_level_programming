b913c0a23755d3aa0bd76ae269718874bb373b55
scardona14 <cardanasean123@gmail.com>
2024-02-01 16:52:44 -0400

    fixed blank lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Text indentation module"""
+
+
 def text_indentation(text):
-    """Prints a text with 2 new lines after each of these characters: ., ? and :
+    """Prints a text with 2 new lines after each characters: ., ? and :
     Args:
         text (str): The text to print.
     Raises:
