a8117884862ba6e79fde835bb62fa7923c2c3dfa
scardona14 <cardanasean123@gmail.com>
2024-02-01 11:25:08 -0400

    fixed errors

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,12 +10,13 @@ class Square:
     def __init__(self, size=0):
         """"
         Define private instance attribute: size
-        Raise TypeError & ValueError if not int or <0 resp"""
+        Raise TypeError & ValueError if not int or <0 resp
+        """
 
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         
         self.__size = size
 
