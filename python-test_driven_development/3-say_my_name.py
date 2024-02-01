a2c02d4587241ffb9545d05220cac44116e01298
scardona14 <cardanasean123@gmail.com>
2024-02-01 15:43:26 -0400

    added expected white lines

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,8 @@
 Function that prints My name is <first name> <last name>
 Raise a TypeError exception if first_name or last_name are not strings
 """
+
+
 def say_my_name(first_name, last_name=""):
     """ This function prints My name is <first name> <last name>
     Raise a TypeError exception if first_name or last_name are not strings
