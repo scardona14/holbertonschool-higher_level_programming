5707aa81f9b0b4e37373e15717078bf5464be69e
scardona14 <cardanasean123@gmail.com>
2024-02-07 15:33:39 -0400

    fixed def function

--- python-everything_is_object/19-copy_list.py
+++ python-everything_is_object/19-copy_list.py
@@ -2,6 +2,6 @@
 """19. Copy a list inside a list"""
 
 
-def copy_list(mylist):
+def copy_list(a_list):
     """Function that returns a copy of a list"""
-    return mylist.copy()
+    return a_list.copy()
