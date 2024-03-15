052840021249815566361321caa747fd347a1606
scardona14 <cardanasean123@gmail.com>
2024-03-15 13:58:34 -0400

    Fixede typo

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,5 @@
 -- A script that creates the table unique_id on your MySQL server
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT  DEFAULT 1 UNIQUE,
-    name VARCHAR(256),
-    score INT
+    name VARCHAR(256)
 );
