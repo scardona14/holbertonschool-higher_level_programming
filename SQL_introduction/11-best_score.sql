3d851f361e87b77c81d84c9cf53bedc57626b85b
scardona14 <cardanasean123@gmail.com>
2024-03-15 10:12:27 -0400

    Fixing the clause

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- Script that list all records with a score >= 10 in a table
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table HAVING score >= 10 ORDER BY score DESC;
 ```
