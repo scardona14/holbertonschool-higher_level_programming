63bbcaff1a6007b5cbb9c9cd62e25b7ce50cd441
scardona14 <cardanasean123@gmail.com>
2024-03-15 14:56:22 -0400

    Fixed typo in line 6

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- A script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
 SELECT g. name
 FROM tv_genres AS g
-INNER JOIN tv_show_genres AS gs
+INNER JOIN tv_show_genres AS s
 ON g. id = gs. genre_id
 INNER JOIN tv_shows AS t
 ON t. id = s. show_id
