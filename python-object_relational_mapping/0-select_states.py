ea29df3217ab8ba9adba1b5c8a96370b90853689
scardona14 <cardanasean123@gmail.com>
2024-03-18 09:56:13 -0400

    Fixed code according to the requirements

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the databse hbtn_0e_0_usa
-"""
-import MySQLdb
+"""A script that lists all states from the database hbtn_0e_0_usa"""
+
 from sys import argv
+import MySQLdb
 
 if __name__ == '__main__':
-    u_name = argv[1]
-    psw = argv[2]
-    base = argv[3]
+    """Function that connects to MySQL server on localhost at port 3306"""
 
-    # Connecting to MySQL database
-    db = MySQLdb.connect(host="localhost", user=u_name,
-                         passwd=psw, db=base, port=3306)
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    # Creating cursor object
     cur = db.cursor()
-
-    # Executing MySql Query
-    cur.execute("SELECT * FROM states ORDER BY id")
-
-    # Obtaining Query Result & prints the result in rows
+    cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
-    # Clean Up
-    cur.close()
-    db.close()
