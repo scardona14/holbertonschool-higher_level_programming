16a0de85c0f9463c5c92ac94a226def74ee18733
scardona14 <cardanasean123@gmail.com>
2024-03-19 20:35:39 -0400

    Script that takes in the name of a state as an argument and lists all cities of that state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""
-A script that takes in the name of a state as an argument
-and lists all cities of that state, using the database hbtn_0e_4_usa
-"""
-
-from sys import argv
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    """Function that connects to MySQL server on localhost at port 3306"""
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
+        exit(1)
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
 
-    with db.cursor() as cur:
-        cur.execute("SELECT cities.id, cities.name, states.name \
-                     FROM cities \
-                     JOIN states \
-                     ON cities.state_id = states.id \
-                     WHERE states.name LIKE BINARY %(state_name)s \
-                     ORDER BY cities.id ASC", {'state_name': argv[4]})
-        rows = cur.fetchall()
-        if rows is not None:
-            result = ", ".join([row[1] for row in rows])
-            print(result)
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
+        cursor = db.cursor()
+        cursor.execute("SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC", (state_name,))
+        cities = cursor.fetchall()
+        if not cities:
+            print("No cities found for the given state.")
+        else:
+            city_names = ', '.join(city[0] for city in cities)
+            print(city_names)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
