5b1285e65fbd835309d99e60719204b2073e1602
scardona14 <cardanasean123@gmail.com>
2024-03-20 11:26:12 -0400

    script that adds the State object “Louisiana”

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""A script that lists all State objects from the database hbtn_0e_6_usa"""
+"""Lists first states object from the database"""
 
-from sqlalchemy.orm import sessionmaker
+
+import sys
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sys import argv
+
 
 if __name__ == "__main__":
-    """Function that connects to MySQL server on localhost at port 3306"""
+    """Lists first state object from the database"""
 
-    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
-                                                          argv[3])
-    engine = create_engine(db)
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{user}:{password}@localhost:3306/{database}"
+    )
+
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_name = "Louisiana"
-    new_state = State(name=new_name)
-    session.add(new_state)
+    state_obj = State(name='Louisiana')
+    session.add(state_obj)
     session.commit()
-    print(new_state.id)
-    session.close()
+    states = session.query(State).distinct().count()
+    print(states)
