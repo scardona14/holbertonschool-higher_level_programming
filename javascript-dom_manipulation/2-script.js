98b45ab1f3a48b669a90afd63fef040c238cd05c
scardona14 <cardanasean123@gmail.com>
2024-05-28 12:32:51 -0400

    Adding `.red` class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,3 +1,5 @@
-$('DIV#red_header').click(function () {
-  $('HEADER').addClass('red');
+const redHeader = document.getElementById('red_header');
+redHeader.addEventListener('click', function() {
+    const header = document.querySelector('header');
+    header.style.color = '#FF0000';
 });
