6017faded2bf5ccac56d56b73fddb33ab1b33959
scardona14 <cardanasean123@gmail.com>
2024-05-28 12:56:25 -0400

    Changing the text

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,3 +1,5 @@
-$('DIV#add_item').click(() => {
-    $('UL.my_list').append('<li>Item</li>');
+const updateHeader = document.getElementById('update_header');
+updateHeader.addEventListener('click', function() {
+    const header = document.querySelector('header');
+    header.textContent = 'New Header!!!';
 });
