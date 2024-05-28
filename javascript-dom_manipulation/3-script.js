538e3e5fb43a0f4773c8bcedc9c70ecd19a093f1
scardona14 <cardanasean123@gmail.com>
2024-05-28 12:35:17 -0400

    Toggling classes

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,11 @@
-$('DIV#red_header').click(() => $('HEADER').addClass('red'));
+const toggleHeader = document.getElementById('toggle_header');
+toggleHeader.addEventListener('click', function() {
+    const header = document.querySelector('header');
+    if (header.classList.contains('red')) {
+        header.classList.remove('red');
+        header.classList.add('green');
+    } else {
+        header.classList.remove('green');
+        header.classList.add('red');
+    }
+});
