0170a7236f930b698928a3ecaeef9609266c410a
scardona14 <cardanasean123@gmail.com>
2024-05-28 13:14:13 -0400

    Star Wars movies

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,5 +1,13 @@
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', (data, textStatus) => {
-  if (textStatus === 'success') {
-    $('#character').text(data.name);
-  }
-});
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+fetch(apiUrl)
+.then(response => response.json())
+.then(data => {
+   const listMovies = document.getElementById('list_movies');
+   data.results.forEach(movie => {
+    const listItem = document.createElement('li');
+    listItem.textContent = movie.title;
+    listMovies.appendChild(listItem);
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
